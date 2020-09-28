5fdb1946f95f95b220180e3da65bc0d5a92dae50
junilhwang <tjsdlf4261@naver.com>
2020-09-28 23:18:20 +0900

    feat: Header 스타일 적용 완료

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,5 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import css from 'styled-jsx/css';
+
+const linkStyle = css.resolve`
+  color: white;
+  font-weight: 800;
+  font-size: 12px;
+  cursor: pointer;
+  line-height: 26px;
+`;
+
+const linkImageStyle = css.resolve`
+  width: 25px;
+  height: 25px;
+  border-radius: 100%;
+  overflow: hidden;
+  margin-right: 5px;
+`;
 
 const Header = () => {
   return (
@@ -9,66 +26,59 @@ const Header = () => {
       </a>
       <ul className="nav">
         <li className="nav-item">
-          <Link to="/login" className="nav-link">
-            {' '}
-            로그인{' '}
+          <Link to="/login" className={`nav-link ${linkStyle.className}`}>
+            로그인
           </Link>
         </li>
         <li className="nav-item">
-          <Link to="/signup" className="nav-link">
-            {' '}
-            회원가입{' '}
+          <Link to="/login" className={`nav-link ${linkStyle.className}`}>
+            Harry
           </Link>
         </li>
         <li className="nav-item">
-          <a href="#!" className="nav-link">
+          <Link to="/signup" className={`nav-link ${linkStyle.className}`}>
+            회원가입
+          </Link>
+        </li>
+        <li className="nav-item">
+          <a href="#!" className={`nav-link ${linkStyle.className}`}>
             <img
+              className={linkImageStyle.className}
               alt="user image"
               src="https://s3.ap-northeast-2.amazonaws.com/grepp-cloudfront/programmers_imgs/learn/course9872/instructor_harry.png"
             />
           </a>
         </li>
         <li className="nav-item">
-          <a href="#!" className="nav-link">
+          <a href="#!" className={`nav-link ${linkStyle.className}`}>
             로그아웃
           </a>
         </li>
       </ul>
-      <style jsx>{`
-        nav.fixed-top {
-          height: 50px;
-        }
-        nav.navbar.bg-blue {
-          background-color: #3b5999;
-        }
-
-        .nav .nav-item .nav-link {
-          color: white;
-          font-weight: 800;
-          font-size: 12px;
-          cursor: pointer;
-          line-height: 26px;
-        }
-
-        .nav .nav-item .nav-link:hover {
-          color: rgba(255, 255, 255, 0.75);
-        }
-
-        .navbar-brand i.fa-facebook-square {
-          font-size: 27px;
-          color: white;
-        }
-
-        .nav-link > img {
-          width: 25px;
-          height: 25px;
-          border-radius: 100%;
-          overflow: hidden;
-          margin-right: 5px;
-        }
-      `}</style>
+      <style jsx>{HeaderStyle}</style>
+      {linkStyle.styles}
+      {linkImageStyle.styles}
     </nav>
   );
 };
+
+const HeaderStyle = css`
+  nav.fixed-top {
+    height: 50px;
+  }
+
+  nav.navbar.bg-blue {
+    background-color: #3b5999;
+  }
+
+  .nav .nav-item .nav-link:hover {
+    color: rgba(255, 255, 255, 0.75);
+  }
+
+  .navbar-brand i.fa-facebook-square {
+    font-size: 27px;
+    color: white;
+  }
+`;
 
 export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import css from 'styled-jsx/css';
 
 const Home = () => {
   return (
@@ -32,35 +33,125 @@ const Home = () => {
           댓글달기
         </button>
       </form>
-      <style jsx>{`
-        .write-form > textarea.form-control {
-          min-height: 100px;
-          line-height: 20px;
-          padding: 20px;
-          font-size: 18px;
-          resize: none;
-          border: none;
-          border-radius: 0.5rem;
-          transition: box-shadow ease-in-out 1s;
-        }
-        .write-form > textarea:focus {
-          box-shadow: #999999 0 0 50px;
-        }
-        .write-form > button.btn {
-          float: right;
-          margin-bottom: 0;
-          margin-top: 16px;
-          background-color: #3b5999;
-          color: #fffffe;
-          border: none;
-          font-weight: 800;
-        }
-        .write-form {
-          margin-bottom: 100px;
-        }
-      `}</style>
+      <style jsx>{HomeStyle}</style>
     </div>
   );
 };
+
+const HomeStyle = css`
+  .write-form > textarea.form-control {
+    min-height: 100px;
+    line-height: 20px;
+    padding: 20px;
+    font-size: 18px;
+    resize: none;
+    border: none;
+    border-radius: 0.5rem;
+    transition: box-shadow ease-in-out 1s;
+  }
+
+  .write-form > textarea:focus {
+    box-shadow: #999999 0 0 50px;
+  }
+
+  .write-form > button.btn {
+    float: right;
+    margin-bottom: 0;
+    margin-top: 16px;
+    background-color: #3b5999;
+    color: #fffffe;
+    border: none;
+    font-weight: 800;
+  }
+
+  .write-form {
+    margin-bottom: 100px;
+  }
+
+  .card {
+    padding: 0;
+    margin-top: 50px;
+    border: none;
+    border-radius: 0.5rem;
+  }
+
+  .card .card-body {
+    padding: 40px;
+  }
+
+  .card .card-text {
+    padding-top: 20px;
+    white-space: pre-wrap;
+  }
+
+  .card .card-info {
+    height: 20px;
+  }
+
+  .card .card-info .thumb-count,
+  .card .card-info .comment-count {
+    display: inline-block;
+    margin-right: 24px;
+    vertical-align: middle;
+    font-size: 12px;
+    cursor: pointer;
+    padding: 0;
+    border: none;
+    background-color: transparent;
+    transition: color ease-in-out 0.3s;
+    transition: margin-top ease-in-out 0.2s;
+  }
+
+  .card .card-info .thumb-count:hover,
+  .card .card-info .comment-count:hover {
+    color: #007bff;
+    margin-top: -3px;
+  }
+
+  .card .card-info .thumb-count .on {
+    color: #007bff;
+  }
+
+  ul.comment-list {
+    padding: 0;
+    list-style: none;
+  }
+
+  li.comment {
+    padding: 20px 40px 24px;
+    border-bottom: 1px solid #e6ecf5;
+    background-color: #fafbfd;
+    position: relative;
+  }
+
+  li.comment:first-child {
+    border-top: 1px solid #e6ecf5;
+  }
+
+  li.comment .comment-text {
+    padding-top: 20px;
+  }
+
+  .comment-form {
+    margin: 20px;
+  }
+
+  .comment-form > textarea.form-control {
+    min-height: 20px;
+    line-height: 20px;
+    border-radius: 0.5rem;
+    resize: none;
+  }
+
+  .comment-form > button.btn {
+    float: right;
+    margin-bottom: 0;
+    margin-top: 16px;
+    background-color: #3b5999;
+    color: #fffffe;
+    border-color: unset;
+    font-weight: 800;
+  }
+`;
 
 export default Home;
