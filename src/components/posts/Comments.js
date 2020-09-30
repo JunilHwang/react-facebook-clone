dec0f16920fc65bec7229670a06d1ac43770a2a6
junilhwang <tjsdlf4261@naver.com>
2020-09-30 21:47:14 +0900

    feat: CommentItem 추가 및 적용

--- src/components/posts/Comments.js
+++ src/components/posts/Comments.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import css from 'styled-jsx/css';
+import CommentItem from './CommentItem';
 
 const Comments = ({ commentList }) => {
   return (
     <>
       <ul className="comment-list">
-        <li className="comment">
-          <div className="comment-info">
-            <h6 className="comment-writer">Aiden</h6>
-            <div className="comment-datetime">10분전</div>
-          </div>
-          <p className="comment-text">그래요 배워야죠 배워야 남는거죠...</p>
-        </li>
+        {commentList.map((comment) => (
+          <CommentItem {...comment} key={comment.seq} />
+        ))}
       </ul>
       <style jsx>{commentStyle}</style>
     </>
@@ -22,21 +19,6 @@ const commentStyle = css`
   ul.comment-list {
     padding: 0;
     list-style: none;
-  }
-
-  li.comment {
-    padding: 20px 40px 24px;
-    border-bottom: 1px solid #e6ecf5;
-    background-color: #fafbfd;
-    position: relative;
-  }
-
-  li.comment:first-child {
-    border-top: 1px solid #e6ecf5;
-  }
-
-  li.comment .comment-text {
-    padding-top: 20px;
   }
 `;
 
