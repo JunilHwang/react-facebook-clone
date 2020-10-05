f30d60e0b6de97155c7a7c13b48da24039d50daf
junilhwang <tjsdlf4261@naver.com>
2020-10-06 01:10:52 +0900

    refactor: storage 추상화 및 session 추가

--- src/storage/index.js
+++
@@ -1,9 +0,0 @@
-export default Object.freeze({
-  get(key, defaultValue = null) {
-    return JSON.parse(localStorage.getItem(key)) || defaultValue;
-  },
-
-  set(key, value) {
-    localStorage.setItem(key, JSON.stringify(value));
-  },
-});
