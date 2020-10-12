566de13830ea2223544b24b6db275157e5d9c44d
junilHwang <tjsdlf4261@naver.com>
2020-10-12 22:37:09 +0900

    fix: header 오류 수정 (3)

--- src/layouts/DefaultLayout.js
+++ src/layouts/DefaultLayout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { Header } from '../components';
+import Header from '../components/header';
 
 const DefaultLayout = ({ component: Component, ...rest }) => {
   return (
