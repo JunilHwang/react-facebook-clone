4d4eb89a3f1bb29ffc0abe2e73dfd4da8ccd33c1
junilHwang <tjsdlf4261@naver.com>
2020-10-12 22:17:29 +0900

    fix: header 오류 수정

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,2 +1,25 @@
-export * from './nav';
-export { default as Header } from './Header';
+import React from 'react';
+import css from 'styled-jsx/css';
+import Logo from './Logo';
+import NavItems from './NavItems';
+
+const Header = (props) => {
+  return (
+    <nav className="navbar fixed-top bg-blue">
+      <Logo />
+      <NavItems {...props} />
+      <style jsx>{HeaderStyle}</style>
+    </nav>
+  );
+};
+
+const HeaderStyle = css`
+  nav.fixed-top {
+    height: 50px;
+  }
+  nav.navbar.bg-blue {
+    background-color: #3b5999;
+  }
+`;
+
+export default Header;
