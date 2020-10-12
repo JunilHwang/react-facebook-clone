564028240454c7bfacda3ee353f30cb6ee1b84be
junilHwang <tjsdlf4261@naver.com>
2020-10-13 01:45:17 +0900

    feat: actionTypes 정의 완료

---
+++ src/data/posts/actionTypes.js
@@ -0,0 +1,3 @@
+export const FETCH_POSTS = 'FETCH_POSTS';
+export const ADD_POST = 'ADD_POST';
+export const TOGGLE_LIKE = 'TOGGLE_LIKE';
---
+++ src/data/users/actionTypes.js
@@ -0,0 +1,3 @@
+export const FETCH_USER = 'FETCH_USER';
+export const ADD_USER = 'ADD_USER';
+export const ADD_USER = 'ADD_USER';
