ddf6305a0322f98e283f493b1a0ce99c2dd817a5
junilHwang <tjsdlf4261@naver.com>
2020-10-14 00:03:01 +0900

    feat: REMOVE_AUTH action 추가

--- src/data/users/actionTypes.js
+++ src/data/users/actionTypes.js
@@ -1,1 +1,2 @@
 export const FETCH_AUTH = 'FETCH_AUTH';
+export const REMOVE_AUTH = 'REMOVE_AUTH';
--- src/data/users/reducers.js
+++ src/data/users/reducers.js
@@ -1,15 +1,20 @@
-import { FETCH_AUTH } from './actionTypes';
+import { FETCH_AUTH, REMOVE_AUTH } from './actionTypes';
 import { userService } from '../../services';
 
 export default (state, { type }) => {
   switch (type) {
     case FETCH_AUTH:
       return {
-        auth: userService.getAuth()
+        auth: userService.getAuth(),
+      };
+    case REMOVE_AUTH:
+      userService.removeAuth();
+      return {
+        auth: null,
       };
     default:
       return {
-        auth: null
+        auth: null,
       };
   }
 };
