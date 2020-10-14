8739d08e97c2b06e20bdb0d3546eb9f6b4133fad
junilHwang <tjsdlf4261@naver.com>
2020-10-14 21:12:57 +0900

    feat: post selector에서 writer로 필터링 하는 기능 추가

--- src/data/posts/selectors.js
+++ src/data/posts/selectors.js
@@ -1,6 +1,12 @@
 import { createSelector } from 'reselect';
 
-export const selectAllPosts = (state) => state.posts;
-export const selectAllPostsOrderByCreateAt = createSelector(selectAllPosts, ({ entries, ids }) =>
-  ids.map((seq) => entries[seq]).sort((a, b) => b.createAt - a.createAt)
-);
+export const selectAllPosts = ({ posts, router }) => {
+  const { writer } = router.location.query;
+  return { ...posts, writerSeq: writer ? Number(writer) : null };
+};
+
+export const selectAllPostsOrderByCreateAt = createSelector(selectAllPosts, ({ entries, ids, writerSeq }) => {
+  const posts = ids.map((seq) => entries[seq]).sort((a, b) => b.createAt - a.createAt);
+  if (writerSeq === null) return posts;
+  return posts.filter(({ writer }) => writer.seq === writerSeq);
+});
