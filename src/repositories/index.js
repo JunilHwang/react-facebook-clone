c499281ca1a35ea7750ee8f3bb035aa514e38289
junilHwang <tjsdlf4261@naver.com>
2020-10-18 03:08:37 +0900

    fix: repository 삭제 (불필요)

--- src/repositories/index.js
+++
@@ -1,6 +0,0 @@
-import Repository from "./Repository";
-import UserRepository from "./UserRepository";
-
-export const postRepository = new Repository('posts');
-export const commentRepository = new Repository('comments');
-export const userRepository = new UserRepository('users');
