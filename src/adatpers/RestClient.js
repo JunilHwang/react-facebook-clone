579e81e92558e34889205445a4d1f457f480c151
junilHwang <tjsdlf4261@naver.com>
2020-10-18 03:08:47 +0900

    feat: RestClient 추가

---
+++ src/adatpers/RestClient.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export default class RestClient {
+
+  constructor({ baseURL, timeout = 5000, headers = {} }, responseMiddleware = ({ data }) => data) {
+    const client = axios.create({ baseURL, timeout, headers });
+    for (const methodName of ['get', 'post', 'put', 'delete', 'patch']) {
+      this[methodName] = (...args) => client[methodName](...args).then(responseMiddleware);
+    }
+  }
+
+}
---
+++ src/adatpers/index.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+const adapter = axios.create({
+  baseURL: 'http://15.164.170.69:8080/',
+});
