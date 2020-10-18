86a8b8113b54ae5203c5d3baccbbf90b15f1b12d
junilHwang <tjsdlf4261@naver.com>
2020-10-19 01:29:28 +0900

    fix: autoHeight가 제대로 작동하지 않는 현상 해결

--- src/hooks/useAutoHeight.js
+++ src/hooks/useAutoHeight.js
@@ -4,10 +4,15 @@ export const useAutoHeight = () => {
   const $contentRef = useRef(null);
 
   useLayoutEffect(() => {
-    const { scrollHeight, clientHeight } = $contentRef.current;
-    if (scrollHeight !== clientHeight) {
-      $contentRef.current.style.height = scrollHeight + 'px';
-    }
+    const handleInput = () => {
+      $contentRef.current.style.height = 'auto';
+      const { scrollHeight, clientHeight } = $contentRef.current;
+      if (scrollHeight !== clientHeight) {
+        $contentRef.current.style.height = scrollHeight + 'px';
+      }
+    };
+    $contentRef?.current?.addEventListener('input', handleInput);
+    return () => $contentRef?.current?.removeEventListener('input', handleInput);
   }, [$contentRef]);
 
   return $contentRef;
