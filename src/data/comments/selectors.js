6f9b528de3ff940fbeba715bc6ff9edadd4c5576
junilHwang <tjsdlf4261@naver.com>
2020-10-18 21:58:31 +0900

    refactor: comments selectors 로직 변경

--- src/data/comments/selectors.js
+++ src/data/comments/selectors.js
@@ -4,5 +4,5 @@ export const selectAllComments = (state) => state.comments;
 export const selectCommentsOfPost = (postId) =>
   createSelector(selectAllComments, (commentsByPost) => {
     const { byId, ids } = commentsByPost[postId] || { byId: {}, ids: [] };
-    return ids.map((id) => byId[id]).sort((a, b) => b.createAt - a.createAt);
+    return ids.sort((a, b) => a - b).map((id) => byId[id]);
   });
