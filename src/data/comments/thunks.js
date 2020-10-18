14e7b0c716240a7e4d4ce6ee378eb7116ba71ce7
junilHwang <tjsdlf4261@naver.com>
2020-10-18 21:58:10 +0900

    refactor: comment thunks를 async/await으로 변경

--- src/data/comments/thunks.js
+++ src/data/comments/thunks.js
@@ -1,14 +1,20 @@
 import { commentService } from '@/services';
 import { setComment, setCommentsOfPost } from './actions';
 
-export const fetchComments = (userId, postId) => (dispatch) => {
-  return commentService
-    .fetchComments(userId, postId)
-    .then((comments) => dispatch(setCommentsOfPost({ postId, comments })));
+export const fetchComments = (userId, postId) => async (dispatch) => {
+  dispatch(
+    setCommentsOfPost({
+      comments: await commentService.fetchComments(userId, postId),
+      postId,
+    })
+  );
 };
 
-export const addComment = (userId, postId, contents) => (dispatch) => {
-  return commentService
-    .addComment(userId, postId, contents)
-    .then((comment) => dispatch(setComment({ postId, comment })));
+export const addComment = (userId, postId, contents) => async (dispatch) => {
+  dispatch(
+    setComment({
+      comment: await commentService.addComment(userId, postId, contents),
+      postId,
+    })
+  );
 };
