a6e8b7d272ec6df11e0234cfbe5ef49b3db8cf60
junilHwang <tjsdlf4261@naver.com>
2020-10-18 21:57:06 +0900

    refactor: try/catch 제거

--- src/data/posts/thunks.js
+++ src/data/posts/thunks.js
@@ -3,35 +3,21 @@ import { setPost, setPosts } from './actions';
 import { setComments } from '@/data/comments/actions';
 
 export const fetchPostsOfUser = (userId) => async (dispatch) => {
-  try {
-    const posts = await postService.fetchPostsOfUser(userId);
-    dispatch(setPosts(posts));
-    const fetchAllCommentsByPost = posts.map(({ seq }) => commentService.fetchComments(userId, seq));
-    dispatch(
-      setComments({
-        postIds: posts.map(({ seq }) => seq),
-        commentsByPost: await Promise.all(fetchAllCommentsByPost),
-      })
-    );
-  } catch (e) {
-    alert(e.message);
-  }
+  const posts = await postService.fetchPostsOfUser(userId);
+  dispatch(setPosts(posts));
+  const fetchAllCommentsOfPost = posts.map(({ seq }) => commentService.fetchComments(userId, seq));
+  dispatch(
+    setComments({
+      postIds: posts.map(({ seq }) => seq),
+      commentsByPost: await Promise.all(fetchAllCommentsOfPost),
+    })
+  );
 };
 
 export const addPost = (userId, contents) => async (dispatch) => {
-  try {
-    const post = await postService.addPost(userId, contents);
-    dispatch(setPost(post));
-  } catch (e) {
-    alert(e.message);
-  }
+  dispatch(setPost(await postService.addPost(userId, contents)));
 };
 
 export const toggleLikePost = (userId, postId) => async (dispatch) => {
-  try {
-    const post = await postService.toggleLike(userId, postId);
-    dispatch(setPost(post));
-  } catch (e) {
-    alert(e.message);
-  }
+  dispatch(setPost(await postService.toggleLike(userId, postId)));
 };
