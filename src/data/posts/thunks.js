1f60016a680cf4e5ed63548f77341a340568396c
junilHwang <tjsdlf4261@naver.com>
2020-10-18 16:58:24 +0900

    feat: post를 불러오는 시점에 comment도 같이 불러오도록 작업

--- src/data/posts/thunks.js
+++ src/data/posts/thunks.js
@@ -1,14 +1,30 @@
-import { postService } from '@/services';
+import { commentService, postService } from '@/services';
 import { setPost, setPosts } from './actions';
+import { setComments } from '@/data/comments/actions';
 
-export const fetchPostsOfUser = (userId) => (dispatch) => {
-  return postService.fetchPostsOfUser(userId).then((posts) => dispatch(setPosts(posts)));
-};
+export const fetchPostsOfUser = (userId) => (dispatch) =>
+  postService
+    .fetchPostsOfUser(userId)
+    .then((posts) => {
+      dispatch(setPosts(posts));
+      const postIds = posts.map(({ seq }) => seq);
+      const fetchAllCommentsByPost = postIds.map((postId) => commentService.fetchComments(userId, postId));
+      return Promise.all(fetchAllCommentsByPost)
+        .then((commentsByPost) => dispatch(setComments({ postIds, commentsByPost })))
+        .catch((e) => {
+          throw new Error(e);
+        });
+    })
+    .catch((e) => alert(e.message));
 
-export const addPost = (userId, contents) => (dispatch) => {
-  return postService.addPost(userId, contents).then((post) => dispatch(setPost(post)));
-};
+export const addPost = (userId, contents) => (dispatch) =>
+  postService
+    .addPost(userId, contents)
+    .then((post) => dispatch(setPost(post)))
+    .catch((e) => alert(e.message));
 
-export const toggleLikePost = (userId, postId) => (dispatch) => {
-  return postService.toggleLike(userId, postId).then((post) => dispatch(setPost(post)));
-};
+export const toggleLikePost = (userId, postId) => (dispatch) =>
+  postService
+    .toggleLike(userId, postId)
+    .then((post) => dispatch(setPost(post)))
+    .catch((e) => alert(e.message));
