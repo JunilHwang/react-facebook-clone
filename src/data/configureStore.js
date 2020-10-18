02da0d6c2bc2a2512036ffaf2ff40195dcedf60a
junilHwang <tjsdlf4261@naver.com>
2020-10-18 12:35:04 +0900

    feat: store에 thunk 연결

--- src/data/configureStore.js
+++ src/data/configureStore.js
@@ -1,13 +1,14 @@
 import { routerMiddleware } from 'connected-react-router';
 import { applyMiddleware, compose, createStore } from 'redux';
 import { createBrowserHistory } from 'history';
+import thunk from 'redux-thunk';
 
 import { createRootReducer } from './rootReducer';
 
 export const history = createBrowserHistory();
 
 export const configureStore = () => {
-  const store = createStore(createRootReducer(history), compose(applyMiddleware(routerMiddleware(history))));
+  const store = createStore(createRootReducer(history), compose(applyMiddleware(routerMiddleware(history), thunk)));
 
   return { store, history };
 };
