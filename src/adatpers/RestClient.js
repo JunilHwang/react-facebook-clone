e2da4894ce987217c7729973fae0c643eb477bd0
junilHwang <tjsdlf4261@naver.com>
2020-10-18 21:25:58 +0900

    refactor: RestClient에서 에러 메세지 핸들링

--- src/adatpers/RestClient.js
+++ src/adatpers/RestClient.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 
 export default class RestClient {
-
   constructor({ baseURL, timeout = 5000, headers = {} }, responseMiddleware = ({ data }) => data) {
     const client = axios.create({ baseURL, timeout, headers });
     for (const methodName of ['get', 'post', 'put', 'delete', 'patch']) {
-      this[methodName] = (...args) => client[methodName](...args).then(responseMiddleware);
+      this[methodName] = (...args) =>
+        client[methodName](...args)
+          .then(responseMiddleware)
+          .catch(({ response: { data } }) => {
+            throw new Error(data.error.message);
+          });
     }
   }
-
 }
