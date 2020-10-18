bbf17434c82e9dd7fdf7f8ef0d9b0955a8232d8a
junilHwang <tjsdlf4261@naver.com>
2020-10-19 00:35:22 +0900

    refactor: 회원가입 과정에 필요한 에러메세지를 관리하는 파일 위치 변경

--- src/pages/SignUp/SignUpErrorMessage.js
+++ src/pages/SignUp/SignUpErrorMessage.js
@@ -1,5 +1,15 @@
 export const EMAIL_BLANK = '이메일을 입력해주세요';
 export const PASSWORD_BLANK = '비밀번호를 입력해주세요';
 export const REPEAT_PASSWORD_BLANK = '비밀번호 확인란을 입력해주세요';
+export const NAME_BLANK = '이름을 입력해주세요';
 export const NONE_MATCH_PASSWORD = '비밀번호와 비밀번호 확인란의 값이 다릅니다.';
 export const EMAIL_EXISTS = '이미 가입된 회원의 이메일 주소입니다.';
+
+export default {
+  EMAIL_BLANK,
+  PASSWORD_BLANK,
+  REPEAT_PASSWORD_BLANK,
+  NAME_BLANK,
+  NONE_MATCH_PASSWORD,
+  EMAIL_EXISTS,
+};
