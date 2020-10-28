a0c2f2739578ed32258bacb1a06088466a7bd7a5
junilHwang <tjsdlf4261@naver.com>
2020-10-29 02:25:27 +0900

    feat: api_token에 대한 interceptor 추가 완료

--- src/services/apis/clients.js
+++ src/services/apis/clients.js
@@ -1,10 +1,19 @@
 import axios from 'axios';
+import { authService } from '@/services';
 
 const SOCIAL_SERVER_URL = 'http://15.164.170.69:8081/api';
 
 const socialApiClient = axios.create({
   baseURL: SOCIAL_SERVER_URL,
   timeout: 5000,
+});
+
+socialApiClient.interceptors.request.use((config) => {
+  const { apiToken } = authService.get();
+  if (apiToken !== null) {
+    config.headers.api_key = `Bearer ${apiToken}`;
+  }
+  return config;
 });
 
 socialApiClient.interceptors.response.use(
