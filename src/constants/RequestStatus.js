6d3a18af5f4aa502c9dc38c0397709f318488b6e
junilHwang <tjsdlf4261@naver.com>
2020-10-29 00:40:10 +0900

    feat: Status에 대한 상수 추가

---
+++ src/constants/RequestStatus.js
@@ -0,0 +1,3 @@
+export const FAIL = 0;
+export const SUCCESS = 1;
+export const LOADING = 2;
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,6 @@
 import * as FormErrorMessage from './FormErrorMessage';
 import * as AuthErrorMessage from './AuthErrorMessage';
 import * as AuthMessage from './AuthMessage';
+import * as RequestStatus from './RequestStatus';
 
-export { FormErrorMessage, AuthErrorMessage, AuthMessage };
+export { FormErrorMessage, AuthErrorMessage, AuthMessage, RequestStatus };
